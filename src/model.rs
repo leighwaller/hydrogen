528610f2e62663cdcb9210d5681cfc7efeb9ee3c
leigh_waller <leigh.waller2@icloud.com>
2018-04-28 23:11:50 +1000

    Added tests for basic models

--- src/model.rs
+++ src/model.rs
@@ -16,13 +16,13 @@ pub struct Simulation {
 
 #[derive(Debug, Deserialize)]
 pub struct HttpEndpoint {
-    #[serde(skip)]
+    #[serde(default = "Uuid::new_v4")]
     pub id: Uuid,
     pub url: String,
     pub method: HttpMethod,
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, PartialEq)]
 pub enum HttpMethod {
     GET,
     POST,
@@ -30,12 +30,47 @@ pub enum HttpMethod {
     PATCH,
 }
 
-pub struct Message<T> {
-    pub id: Uuid,
-    pub payload: T,
-}
+//pub struct Message<T> {
+//    pub id: Uuid,
+//    pub payload: T,
+//}
 
-pub enum Command {
-    Run,
-    Check,
+//pub enum Command {
+//    Run,
+//    Check,
+//}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+    use toml;
+
+    #[test]
+    fn should_deserialize_simulation() {
+        let simulation: Simulation = toml::from_str(r#"
+            user_count = 1
+            fail_on_error = false
+            [endpoint]
+            method = 'GET'
+            url = 'http://localhost:8080/test'
+        "#).unwrap();
+
+        assert_ne!(simulation.id, Uuid::nil());
+        assert_eq!(simulation.user_count, 1);
+        assert_eq!(simulation.endpoint.url, "http://localhost:8080/test");
+    }
+
+    #[test]
+    fn should_deserialize_endpoint() {
+        let endpoint: HttpEndpoint = toml::from_str(r#"
+            url = 'http://test.com.au'
+            method = 'POST'
+        "#).unwrap();
+
+        assert_ne!(endpoint.id, Uuid::nil());
+        assert_eq!(endpoint.url, "http://test.com.au");
+        assert_eq!(endpoint.method, HttpMethod::POST);
+    }
+
 }
